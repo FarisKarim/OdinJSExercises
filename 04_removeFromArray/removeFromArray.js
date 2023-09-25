52eb849018b2c885b1fdb527d7f37e1da01888b1
Faris <fariskarim2000@gmail.com>
2023-09-24 20:55:17 -0400

    up to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, numToRemove) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] == numToRemove) {
+            list.splice(i, 1)
+            break
+        }
+    }
+    return list
 };
 
 // Do not edit below this line
